189fa05fe12d936e213794ef8b5fde97e8766fcc
Ben Matthies <matthiesbe@gmail.com>
2025-05-20 20:21:46 +0200

    Home config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,3 +1,56 @@
-{ ... }: {
+{ pkgs, ... }: {
+  imports = [
+    ./editor
+    ./graphical
+  ];
+
+  home.username = "ben";
+  home.homeDirectory = "/home/ben";
+
+  home.packages = with pkgs; [
+    htop
+    fzf
+    fd
+    ripgrep
+    fastfetch
+    inetutils
+    gh
+  ];
+
+  programs = {
+    home-manager.enable = true;
+
+    bash.enable = true;
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "junkfood";
+        plugins = [
+          "tmux"
+          "git"
+          "ssh"
+          "fzf"
+        ];
+      };
+    };
+
+    tmux = {
+      enable = true;
+      keyMode = "vi";
+      shortcut = "a";
+      mouse = true;
+    };
+
+    git = {
+      enable = true;
+      extraConfig = {
+        user.name = "Ben Matthies";
+        user.email = "matthiesbe@gmail.com";
+        init.defaultBranch = "main";
+      };
+    };
+  };
+
   home.stateVersion = "24.11";
 }
