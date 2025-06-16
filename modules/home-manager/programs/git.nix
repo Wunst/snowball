cfcbce77158785425e3222421ec2f986343eb1db
Ben Matthies <matthiesbe@gmail.com>
2025-06-16 23:03:41 +0200

    refactor: git, gh without hosts.yml

---
+++ modules/home-manager/programs/git.nix
@@ -0,0 +1,54 @@
+{ config, lib, ... }: let
+  cfg = config.programs.sn-git;
+in {
+  options = {
+    programs.sn-git = 
+      with lib; {
+        enable = mkEnableOption "Enable and configure git";
+
+        user = {
+          name = mkOption {
+            type = str;
+            description = "Git user name";
+          };
+
+          email = mkOption {
+            type = str;
+            description = "Git user email";
+          };
+        };
+
+        gh.user = mkOption {
+          type = str;
+          description = "GitHub user name";
+        };
+      };
+  };
+
+  config = {
+    programs = {
+      git = {
+        enable = true;
+
+        config = {
+          init.defaultBranch = "main";
+
+          user = {
+            username = cfg.user.name;
+            email = cfg.user.email;
+          };
+        };
+      };
+
+      gh = {
+        enable = true;
+        hosts = {
+          "github.com" = {
+            # TODO: configure oauth_token, supply via GH_TOKEN, or leave be?
+            user = cfg.gh.user;
+          };
+        };
+      };
+    };
+  };
+}
