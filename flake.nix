5f6cc6e978bf44abd50c81c1124b2b1eb74fc0ca
Ben Matthies <matthiesbe@gmail.com>
2025-08-07 21:17:57 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Ben's NixOS + home-manager configuration";
+
+  inputs = {
+    # I previously used a `git+https` URL to shallow clone here, but the `github` URL syntax already uses the tarball to speed up downloads.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux"; # Used for the devShell.
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default =
+        with pkgs;
+        mkShell {
+          buildInputs = [
+            just
+            nil # Nix language server.
+          ];
+
+          shellHook = ''
+            echo Installing pre-commit hook…
+            cp --preserve=mode pre-commit-hook .git/hooks/pre-commit
+          '';
+        };
+
+      # Configure `nix fmt`.
+      formatter.${system} = pkgs.nixfmt-tree;
+    };
+}
