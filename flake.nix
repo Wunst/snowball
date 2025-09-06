26386682ef38ba7d4b86a60abb9dc53cbdc19184
Ben Matthies <matthiesbe@gmail.com>
2025-09-06 15:51:55 +0200

    Google calendar account

--- flake.nix
+++ flake.nix
@@ -190,90 +190,101 @@
                   };
 
                   # Mail accounts.
-                  accounts.email.accounts = {
-                    "1 personal" = {
-                      primary = true;
-                      address = "matthiesbe@gmail.com";
-                      realName = "Ben Matthies";
-                      flavor = "gmail.com";
-                      gpg = {
-                        key = "015E6B992731C5B8";
-                        encryptByDefault = true;
-                        signByDefault = true;
-                      };
-                      signature = {
-                        showSignature = "append";
-                        delimiter = "~*~-~*~-~*~-~*~";
-                        text = ''
-                          Ben Matthies
-                          -
-                          Sie können mir mit OpenPGP verschlüsselte Nachrichten schicken. Sie sollten
-                          Ihre Nachrichten verschlüsseln, wenn Sie Bedenken haben, dass Ihr oder mein 
-                          Mailanbieter (Google Mail) Ihre Nachricht mitliest.
-                          Hilfe für Ihr Mailprogramm: https://www.openpgp.org/software/
-                          Mein Public Key: https://keys.openpgp.org/search?q=matthiesbe@gmail.com
-                          Fingerprint: 55C7 85F0 91A0 9C19 B096 53A5 015E 6B99 2731 C5B8
+                  accounts = {
+                    email.accounts = {
+                      "1 personal" = {
+                        primary = true;
+                        address = "matthiesbe@gmail.com";
+                        realName = "Ben Matthies";
+                        flavor = "gmail.com";
+                        gpg = {
+                          key = "015E6B992731C5B8";
+                          encryptByDefault = true;
+                          signByDefault = true;
+                        };
+                        signature = {
+                          showSignature = "append";
+                          delimiter = "~*~-~*~-~*~-~*~";
+                          text = ''
+                            Ben Matthies
+                            -
+                            Sie können mir mit OpenPGP verschlüsselte Nachrichten schicken. Sie sollten
+                            Ihre Nachrichten verschlüsseln, wenn Sie Bedenken haben, dass Ihr oder mein 
+                            Mailanbieter (Google Mail) Ihre Nachricht mitliest.
+                            Hilfe für Ihr Mailprogramm: https://www.openpgp.org/software/
+                            Mein Public Key: https://keys.openpgp.org/search?q=matthiesbe@gmail.com
+                            Fingerprint: 55C7 85F0 91A0 9C19 B096 53A5 015E 6B99 2731 C5B8
 
-                          Damit ich Ihnen verschlüsselt antworten kann, müssen Sie Ihren eigenen (von
-                          Ihnen erzeugten) Public Key entweder mitschicken oder auf keys.openpgp.org 
-                          registrieren. '';
-                      };
-                      thunderbird = {
-                        enable = true;
-                        settings = id: {
-                          # More sane archive location than Gmail "All messages"
-                          "mail.identity.id_${id}.archive_folder" = "imap://matthiesbe%40gmail.com@imap.gmail.com/Archiv";
+                            Damit ich Ihnen verschlüsselt antworten kann, müssen Sie Ihren eigenen (von
+                            Ihnen erzeugten) Public Key entweder mitschicken oder auf keys.openpgp.org 
+                            registrieren. '';
+                        };
+                        thunderbird = {
+                          enable = true;
+                          settings = id: {
+                            # More sane archive location than Gmail "All messages"
+                            "mail.identity.id_${id}.archive_folder" = "imap://matthiesbe%40gmail.com@imap.gmail.com/Archiv";
+                          };
                         };
                       };
-                    };
 
-                    "2 school" = {
-                      address = "ben.matthies@mps-ki.de";
-                      realName = "Ben Matthies";
-                      userName = "ben.matthies";
-                      smtp = {
-                        host = "mps-ki.de";
-                        port = 465;
+                      "2 school" = {
+                        address = "ben.matthies@mps-ki.de";
+                        realName = "Ben Matthies";
+                        userName = "ben.matthies";
+                        smtp = {
+                          host = "mps-ki.de";
+                          port = 465;
+                        };
+                        imap = {
+                          host = "mps-ki.de";
+                          port = 993;
+                        };
+                        signature = {
+                          showSignature = "append";
+                          text = ''
+                            Ben Matthies
+                            Admin AG
+                            Max-Planck-Schule Kiel
+                            Winterbeker Weg 1, 24114 Kiel '';
+                        };
+                        thunderbird.enable = true;
                       };
-                      imap = {
-                        host = "mps-ki.de";
-                        port = 993;
-                      };
-                      signature = {
-                        showSignature = "append";
-                        text = ''
-                          Ben Matthies
-                          Admin AG
-                          Max-Planck-Schule Kiel
-                          Winterbeker Weg 1, 24114 Kiel '';
-                      };
-                      thunderbird.enable = true;
-                    };
 
-                    "3 uni" = {
-                      address = "ben.matthies@stu.uni-kiel.de";
-                      realName = "Ben Matthies";
-                      userName = "stu249890";
-                      gpg = {
-                        key = "995B0CAE47D0D637";
-                        encryptByDefault = true;
-                        signByDefault = true;
+                      "3 uni" = {
+                        address = "ben.matthies@stu.uni-kiel.de";
+                        realName = "Ben Matthies";
+                        userName = "stu249890";
+                        gpg = {
+                          key = "995B0CAE47D0D637";
+                          encryptByDefault = true;
+                          signByDefault = true;
+                        };
+                        smtp = {
+                          host = "smtps.mail.uni-kiel.de";
+                          port = 465;
+                        };
+                        imap = {
+                          host = "imap.mail.uni-kiel.de";
+                          port = 993;
+                        };
+                        signature = {
+                          showSignature = "append";
+                          text = ''
+                            Ben Matthies
+                            Student der Physik (1Ba)
+                            3. Fachsemester
+                          '';
+                        };
+                        thunderbird.enable = true;
                       };
-                      smtp = {
-                        host = "smtps.mail.uni-kiel.de";
-                        port = 465;
-                      };
-                      imap = {
-                        host = "imap.mail.uni-kiel.de";
-                        port = 993;
-                      };
-                      signature = {
-                        showSignature = "append";
-                        text = ''
-                          Ben Matthies
-                          Student der Physik (1Ba)
-                          3. Fachsemester
-                        '';
+                    };
+                    calendar.accounts."1 personal" = {
+                      primary = true;
+                      remote = {
+                        type = "caldav";
+                        url = "https://apidata.googleusercontent.com/caldav/v2/chiispr5e9mm2rh3d1nmoqb4c5sk0pridtqn0bjm5phm2r35dpi62shectnmuprcckn66rrd%40virtual/events/";
+                        userName = "matthiesbe@gmail.com";
                       };
                       thunderbird.enable = true;
                     };
