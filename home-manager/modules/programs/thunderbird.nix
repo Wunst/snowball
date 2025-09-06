56a6ae19c7f11db49c92d81fe2c8f0c394fa25ab
Ben Matthies <matthiesbe@gmail.com>
2025-09-06 10:41:59 +0200

    Add mail configuration

--- home-manager/modules/programs/thunderbird.nix
+++ home-manager/modules/programs/thunderbird.nix
@@ -14,6 +14,10 @@ in
       profiles.default = {
         isDefault = true;
         settings = {
+          # Use my Nix managed GPG keys.
+          "mail.openpgp.allow_external_gnupg" = true;
+          "mail.openpgp.fetch_pubkeys_from_gnupg" = true;
+
           # TODO: Configure spellcheck for English and German.
           # Disable spellcheck.
           "extensions.ui.dictionary.hidden" = true;
